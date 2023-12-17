c798d1635be35882916ec843fd28f13f9a4033b9
Shreyance Bodh <shreyancebodh2013@gmail.com>
2023-12-17 10:59:53 +0530

    Lec-33

---
+++ Lec-33/E-commerce/app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const Product = require('./models/product');
+const methodOverride = require('method-override');
+
+mongoose.connect('mongodb://127.0.0.1:27017/E-com-6Aug')
+    .then(() => console.log('E-com-6Aug connected'))
+    .catch(err => console.log(err));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', ejsMate);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+    res.send('Working Fine!!');
+})
+
+// ------------------- product routes
+app.get('/products', async (req, res) => {
+    const products = await Product.find({});
+    res.render('products/index', { products });
+})
+
+app.get('/product/new', (req, res) => {
+    res.render('products/new');
+})
+
+app.post('/products', async (req, res) => {
+    const { name, price, image, desc } = req.body;
+    await Product.create({ name, price, image, desc });
+
+    res.redirect('/products');
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/show', { product });
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product });
+})
+
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+
+    res.redirect('/products');
+})
+
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log('Server is up at port', PORT);
+})
